631c554481baea8c71e078a2171601c91025c31f
shaej <shaej@DESKTOP-O6B1DH7>
2023-11-21 14:04:32 -0600

    Got continue button to work selecting save files from your directory

--- PokemonGame/src/Overworld/WelcomeMenu.java
+++ PokemonGame/src/Overworld/WelcomeMenu.java
@@ -3,6 +3,13 @@ package Overworld;
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Random;
+
 import javax.imageio.ImageIO;
 import Swing.Battle.JGradientButton;
 
@@ -25,7 +32,9 @@ public class WelcomeMenu extends JPanel {
 		setLayout(new BorderLayout());
 		
 		try {
-            img = ImageIO.read(getClass().getResourceAsStream("/gen/background.png"));
+			Random rand = new Random();
+			int random = rand.nextInt(2) + 1;
+            img = ImageIO.read(getClass().getResourceAsStream("/gen/background" + random + ".png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -41,9 +50,6 @@ public class WelcomeMenu extends JPanel {
         optionsPanel = new JPanel(new GridLayout(2, 2));
         
         JGradientButton continueButton = new JGradientButton("Continue");
-        continueButton.addActionListener(e -> {
-        	Main.loadSave(window, gp, "player", this);
-        });
         optionsPanel.add(continueButton);
         
         JGradientButton newGameButton = new JGradientButton("New Game");
@@ -52,11 +58,21 @@ public class WelcomeMenu extends JPanel {
         });
         optionsPanel.add(newGameButton);
         
-        JTextField fileName = new JTextField("player");
+        JComboBox<String> fileName = new JComboBox<>(getDatFiles());
         optionsPanel.add(fileName);
         
         JGradientButton manageButton = new JGradientButton("Manage");
         optionsPanel.add(manageButton);
+        
+        continueButton.addActionListener(e -> {
+        	String save = (String) fileName.getSelectedItem();
+        	if (save == null) {
+        		JOptionPane.showMessageDialog(this, "No save to load from!");
+        		return;
+        	}
+        	gp.player.currentSave = save;
+        	Main.loadSave(window, gp, save, this);
+        });
         
         add(titleLabel, BorderLayout.NORTH);
         add(subtitleLabel, BorderLayout.CENTER);
@@ -64,6 +80,23 @@ public class WelcomeMenu extends JPanel {
         
 	}
 	
+	private String[] getDatFiles() {
+		ArrayList<String> fileNames = new ArrayList<>();
+
+        // Get the current working directory
+        String currentDirectory = System.getProperty("user.dir");
+
+        try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Paths.get(currentDirectory), "*.dat")) {
+            for (Path path : directoryStream) {
+                fileNames.add(path.getFileName().toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return fileNames.toArray(new String[0]);
+	}
+
 	public void paintComponent(Graphics g) {
 		g.drawImage(img, 0, 0, null);
 	}
