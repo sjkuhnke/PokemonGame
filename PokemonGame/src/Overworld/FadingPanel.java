3121777dc4e1e6cfe4b4a2445be6ddceba203f55
shaej <shaej@DESKTOP-O6B1DH7>
2023-11-21 13:29:24 -0600

    Added a welcome menu and have continue work for the default file name

---
+++ PokemonGame/src/Overworld/FadingPanel.java
@@ -0,0 +1,42 @@
+package Overworld;
+
+import java.awt.AlphaComposite;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+public class FadingPanel extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+		private float alpha;
+		
+		public FadingPanel() {
+			alpha = 0.0f;
+		}
+		
+		public float getAlpha() {
+			return alpha;
+		}
+		
+		public void setAlpha(float alpha) {
+			this.alpha = alpha;
+			repaint();
+		}
+		
+		protected void paintComponent(Graphics g) {
+	        super.paintComponent(g);
+	        Graphics2D g2d = (Graphics2D) g.create();
+	        try {
+		        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
+		        g2d.setColor(getBackground());
+		        g2d.fillRect(0, 0, getWidth(), getHeight());
+		        g2d.dispose();
+	        } catch (IllegalArgumentException e) {
+	        	System.out.println(alpha + " is out of range");
+	        	e.printStackTrace();
+	        }
+	    }
+	}
