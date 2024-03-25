7cdb371923de7cb92d61bd08af98899d74cf8154
shaej <shaej@DESKTOP-O6B1DH7>
2024-03-25 14:38:15 -0500

    Redid FadingPanel to basically just be a JPanel with a counter field

--- PokemonGame/src/Overworld/FadingPanel.java
+++ PokemonGame/src/Overworld/FadingPanel.java
@@ -1,9 +1,5 @@
 package Overworld;
 
-import java.awt.AlphaComposite;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-
 import javax.swing.JPanel;
 
 public class FadingPanel extends JPanel {
