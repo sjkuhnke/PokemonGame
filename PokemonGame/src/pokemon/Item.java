f73ff9c8fbb4afbc1a3628f948492330d0013d37
Shae Kuhnke <sjkuhnke@uwm.edu>
2024-08-06 00:50:56 -0500

    Work on maps for Gym 2 split

--- PokemonGame/src/pokemon/Item.java
+++ PokemonGame/src/pokemon/Item.java
@@ -298,11 +298,11 @@ public enum Item {
 	STARF_BERRY(257,500,200,new Color(184, 232, 152),Item.BERRY,null,"If a Pokemon holds one of these Berries, one of its stats will be sharply boosted should it find itself in a pinch."),
 	MICLE_BERRY(258,200,40,new Color(64, 200, 64),Item.BERRY,null,"If a Pokemon holds one of these Berries, its accuracy will be boosted should it find itself in a pinch."),
 	CUSTAP_BERRY(259,200,40,new Color(220, 96, 70),Item.BERRY,null,"If a Pokemon holds one of these Berries, it will be able to act faster in a pinch—but only for the next move."),
-	PACKAGE_A(260,0,0,Color.BLACK,Item.KEY_ITEM,null,"A letter to be delivered to the following recipient:\nExp Lady\nPoppy Grove"),
-	PACKAGE_B(261,0,0,Color.BLACK,Item.KEY_ITEM,null,"A letter to be delivered to the following recipient:\nChef Eddie\nPoppy Grove"),
-	PACKAGE_C(262,0,0,Color.BLACK,Item.KEY_ITEM,null,"A letter to be delivered to the following business:\nThe Pound\nPoppy Grove"),
-	PACKAGE_D(266,0,0,Color.BLACK,Item.KEY_ITEM,null,"A letter to be delivered to the following recipient:\nThe Warehouse\nPoppy Grove"),
-	WAREHOUSE_KEY(267,0,0,Color.BLACK,Item.KEY_ITEM,null,"A key that unlocks Poppy Grove's Warehouse"),
+	PACKAGE_A(260,0,0,Color.BLACK,Item.KEY_ITEM,null,"A large industrial juicer, designed to make berries into healthy juice quickly and effectively. Belongs to one Ms. Plum."),
+	PACKAGE_B(261,0,0,Color.BLACK,Item.KEY_ITEM,null,"Contains a new state of the art frying pan, meant for a master chef. It's said to be for a Guy Eddie."),
+	PACKAGE_C(262,0,0,Color.BLACK,Item.KEY_ITEM,null,"Inside is a bunch of dog toys, and the address says it's for a place called \"Poundtown\"."),
+	PACKAGE_D(266,0,0,Color.BLACK,Item.KEY_ITEM,null,"A Drednaw stapler, for an old friend of Robin's. Sent all the way from Galar, the deliver address is for the local warehouse."),
+	WAREHOUSE_KEY(267,0,0,Color.BLACK,Item.KEY_ITEM,null,"A key that unlocks Poppy Grove's Warehouse."),
 	;
 	
 	private int id;
