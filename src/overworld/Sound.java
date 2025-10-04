027947ccd887693e6c5cd7eb47f3b6ffa873ce3e
sjkuhnke <sjkuhnke@uwm.edu>
2025-10-03 23:02:45 -0500

    Started on the sound engine!

---
+++ src/overworld/Sound.java
@@ -0,0 +1,41 @@
+package overworld;
+
+import java.net.URL;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class Sound {
+	
+	Clip clip;
+	URL[] soundURL = new URL[30];
+	
+	public Sound() {
+		soundURL[0] = getClass().getResource("/sound/videogame1-1.wav");
+		soundURL[1] = getClass().getResource("/sound/videogame1-2.wav");
+		soundURL[2] = getClass().getResource("/sound/videogame1-3.wav");
+	}
+	
+	public void setFile(int i) {
+		try {
+			AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL[i]);
+			clip = AudioSystem.getClip();
+			clip.open(ais);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void play() {
+		clip.start();
+	}
+	
+	public void loop() {
+		clip.loop(Clip.LOOP_CONTINUOUSLY);
+	}
+	
+	public void stop() {
+		clip.stop();
+	}
+}
